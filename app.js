374953c68a7d64d4cac6323d83aa3ee37797703c
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-10-11 12:11:39 +0300

    scraping wiki done

--- app.js
+++ app.js
@@ -16,22 +16,12 @@ async function run(){
     await page.keyboard.type('ethiopia');
 
    await page.keyboard.press('Enter');
-   //await page.waitForSelector('#search-form');
-
-   //await page.click('input[type="submit"]');
 
    await page.waitForNavigation({waitUntil:'networkidle2'});
 
    console.log("Search done successfully!");
    
    await page.screenshot({path: 'wiki.png',fullPage:true});
-
-//    await page.waitForSelector('#mw-content-text');
-//    const text = await page.evaluate(() => {
-//        const anchor = document.querySelector('#mw-content-text');
-//        return anchor.textContent;
-//    });
-//    console.log(text);
 
     await browser.close();
 }
